5d03aeefc49dec6449cfb1c391f6d438e4eeba9f
Md Mohibullah <mdmohibullah352@gmail.com>
2026-06-27 00:02:43 +0600

    added looping in jsx with map method

--- first react/src/main.jsx
+++ first react/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 import App from './App.jsx'
-
+import NetflixSeries from './components/NetflixSeries.jsx'
+// import seriesData from "./api/seriesData.json"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    {/* < NetflixSeries /> */}
   </StrictMode>,
 )
